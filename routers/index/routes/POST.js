32ff4b657d17058deded2ec5417e10b29f46da21
Max Lehmann <maxlehmann1404@gmail.com>
2024-05-05 00:22:00 +0200

    chore: Update IP handling in POST route for index router

--- routers/index/routes/POST.js
+++ routers/index/routes/POST.js
@@ -7,9 +7,11 @@ import Visit from "../../../schemas/Visit.js";
 /* Route-Definition */
 export default async (req, res, next) => {
   try {
-    if(req.ip === '::1') return res.sendStatus(200);
+    const ip = req.headers['x-real-ip'] || req.ip;
 
-    const responseIpInfo = await axios.get(`https://ipinfo.io/${req.ip}?token=${process.env.IPINFO_TOKEN}`);
+    if(ip === "::1") return res.sendStatus(200);
+
+    const responseIpInfo = await axios.get(`https://ipinfo.io/${ip}?token=${process.env.IPINFO_TOKEN}`);
 
     const visit = new Visit(responseIpInfo.data);
     await visit.save();
