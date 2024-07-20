9ede025d8c2a204933c2b76932e6b2513abf4155
Max Lehmann <85064456+MaxLehmann01@users.noreply.github.com>
2024-07-20 21:10:31 +0200

    jwtMiddleware.js aktualisieren

--- middleware/jwtMiddleware.js
+++ middleware/jwtMiddleware.js
@@ -6,6 +6,8 @@ export default async (req, res, next) => {
   try {
     if(!req.cookies['JWT_AT']) return res.status(401).json({ err: 'jwt-at'});
 
+    console.log(req.cookies['JWT_AT']);
+
     jwt.verify(req.cookies['JWT_AT'], process.env.JWT_AT_SECRET, (err, jwtVerified) => {
       if(err) return res.status(403).json({ err: 'jwt-verify'});
 
